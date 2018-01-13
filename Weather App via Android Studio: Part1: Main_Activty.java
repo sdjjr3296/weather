c48a5e314d6e07b21cc17bbc5b342188d9bac519
Jaime Ruelas <sdjjr3296@gmail.com>
2018-01-12 23:15:30 -0800

    Create Weather App via Android Studio: Part1: Main_Activty.java

---
+++ Weather App via Android Studio: Part1: Main_Activty.java
@@ -0,0 +1,85 @@
+package com.example.android.weatherapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+
+/**************** Ways to Make Weather App Project **************/
+// 1- Designing The Layout
+    // 2- Declaring the Widgets
+    // 3- Understanding how to get the Data in order to make it work
+    // 4- Making objects + clarify the Data
+    // 5- Testing !!
+
+    // To get the correct API Key from Website
+    // a- register a free account
+    // b- click on Generate Key
+    // c- No will work on this link to get all Data & Weather Info
+
+    // Insert here the API Key:
+    //
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText cityField;
+    TextView resultWeather;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        cityField = (EditText) findViewById(R.id.editText);
+        resultWeather = (TextView) findViewById(R.id.textView2);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
